45e982ff0db5dd6ae2f4e99fc8775d58bdfba700
Alex <ronaldalexandervacca@unicesar.edu.co>
2022-09-20 21:21:27 -0500

    V2.0 Ecommerce

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app=Flask(__name__)
 
@@ -10,6 +10,40 @@ def index():
 @app.route('/login')
 def login():
     return render_template('login.html')
+
+@app.route('/registro')
+def registro():
+    return render_template('registro.html')
+
+@app.route('/menu')
+def menu():
+    return render_template('menu.html')
+
+@app.route('/politicas')
+def politicas():
+    return render_template('politicas.html')
+
+
+@app.route('/usuarios')
+def menu_user():
+    return render_template('usuarios.html')
+
+@app.route('/productos')
+def menu_productos():
+    return render_template('productos.html')
+
+@app.route('/carrito')
+def menu_carrito():
+    return render_template('carrito.html')
+
+@app.route('/deseos')
+def menu_deseos():
+    return render_template('deseos.html')
+
+@app.route('/gestion')
+def menu_gestion():
+    return render_template('gestionproducto.html')
+
 
 
 if  __name__=='__main__':
